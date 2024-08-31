e95c82edb086f170926b2c4f2167d507981c31ce
Cayzhed <bizard.pierre@hotmail.fr>
2024-08-31 23:01:17 +0200

    test

---
+++ app.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // use the variable if it's defined, otherwise use the fallback
+    icon: process.env.APP_ICON || './assets/icon.png',
+    name: process.env.APP_NAME || 'ViviLinkApp',
+  };
+  
