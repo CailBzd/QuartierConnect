ea126aa4641876a22ed6edd132b5eb50c8b60170
Cayzhed <bizard.pierre@hotmail.fr>
2024-09-02 21:56:43 +0200

    Liaison avec le server et récupération des données (en local)

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Button, Card, Title, Paragraph } from "react-native-paper";
 import { useTranslation } from 'react-i18next';
+import axios from 'axios';
 
 export default function Index() {
   const { t } = useTranslation();
+  const [organizations, setOrganizations] = useState([]);
+
+  useEffect(() => {
+    // Faire une requête à l'API pour récupérer le nom de l'organisation
+    // axios.get('http://192.168.192.1:7788/organizations')
+    axios.get('http://192.168.1.59:7788/organizations')
+      .then(response => {
+        console.log(response);
+        setOrganizations(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching organization name:', error);
+      });
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -19,6 +34,12 @@ export default function Index() {
           </Button>
         </Card.Content>
       </Card>
+      
+      {organizations.map(org => (
+        <Text key={org.id} style={styles.organizationName}>
+          {org.name}
+        </Text>
+         ))}
     </View>
   );
 }
@@ -44,6 +65,10 @@ const styles = StyleSheet.create({
     color: "#333",
     textAlign: "center",
     marginBottom: 10,
+  },
+  organizationName: {
+    fontSize: 18,
+    marginVertical: 5,
   },
   tagline: {
     fontSize: 16,
